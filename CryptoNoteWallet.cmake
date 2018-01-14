88f222823b3683c793b1df0ae90a3aec63d2196e
Alexander Eichhorn <alex@kidtsunami.com>
2018-01-14 18:40:21 +0100

    refactor to cmake-only build system

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,6 +1,25 @@
-
-set(CN_PROJECT_NAME "ultranotewallet")
+set(CN_PROJECT_NAME "UltraNoteWallet")
 set(CN_CURRENCY_DISPLAY_NAME "UltraNote")
 set(CN_CURRENCY_TICKER "XUN")
-set(CN_VERSION 1.0.4)
-set(CN_VERSION_RELEASE beta)
+set(CN_VERSION 1.0.4-beta)
+set(CN_COPYRIGHT "Copyright (c) 2017-2018 UltraNote")
+set(CN_VENDOR "UltraNote Foundation")
+set(CN_IDENTIFIER "org.ultranote.UltraNoteWallet")
+set(CN_CONTACT "https://ultranote.org/")
+set(CN_SUMMARY "UltraNote XUN wallet")
+set(CN_DESCRIPTION "UltraNote XUN wallet\
+ UltraNote is a decentralized privacy protected peer-to-peer\
+ cryptocurrency and untraceable encrypted messaging system. UltraNote is\
+ open-source; its design is public, nobody owns or controls UltraNote and\
+ everyone can take part.\
+ .\
+ Features of the UltraNote wallet listed bellow:\
+ * Classic Bitcoin alike appearance in CryptoNote based cryptocurrency,\
+   made with QT and open-source.\
+ * Instant anonymous XUN transfers and transfers notifications\
+ * Instant untraceable encrypted messages transfers and notifications\
+ * Safe and secure, full UltraNote network node in wallet, fast network\
+   syncronization.\
+ * Wallet encryption\
+ * Fast and hardware optimized\
+ * Open and easy. Private keys can be exported into other clients.")
