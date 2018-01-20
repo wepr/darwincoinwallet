65dbc6ee37e480e4eb9fbecf4b7bcd824297c73d
Alexander Eichhorn <alex@kidtsunami.com>
2018-01-20 15:03:08 +0100

    update branding

--- UltraNoteWallet.cmake
+++ UltraNoteWallet.cmake
@@ -1,13 +1,13 @@
-set(CN_PROJECT_NAME "UltraNoteWallet")
-set(CN_CURRENCY_DISPLAY_NAME "UltraNote")
-set(CN_CURRENCY_TICKER "XUN")
-set(CN_VERSION 1.0.4-beta)
-set(CN_COPYRIGHT "Copyright (c) 2017-2018 UltraNote")
-set(CN_VENDOR "UltraNote Foundation")
-set(CN_IDENTIFIER "org.ultranote.UltraNoteWallet")
-set(CN_CONTACT "https://ultranote.org/")
-set(CN_SUMMARY "UltraNote XUN wallet")
-set(CN_DESCRIPTION "UltraNote XUN wallet\
+set(UN_PROJECT_NAME "UltraNoteWallet")
+set(UN_CURRENCY_DISPLAY_NAME "UltraNote")
+set(UN_CURRENCY_TICKER "XUN")
+set(UN_VERSION 1.0.4-beta)
+set(UN_COPYRIGHT "Copyright (c) 2017-2018 UltraNote")
+set(UN_VENDOR "UltraNote Foundation")
+set(UN_IDENTIFIER "org.ultranote.UltraNoteWallet")
+set(UN_CONTACT "https://ultranote.org/")
+set(UN_SUMMARY "UltraNote XUN wallet")
+set(UN_DESCRIPTION "UltraNote XUN wallet\
  UltraNote is a decentralized privacy protected peer-to-peer\
  cryptocurrency and untraceable encrypted messaging system. UltraNote is\
  open-source; its design is public, nobody owns or controls UltraNote and\
