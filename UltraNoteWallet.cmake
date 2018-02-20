92b4ba9d92f82e2ef5ad69893abf2f61e176ac86
wepr <we-pr@yandex.ru>
2018-02-20 16:31:58 +0200

    Update UltraNoteWallet.cmake

--- UltraNoteWallet.cmake
+++ UltraNoteWallet.cmake
@@ -1,16 +1,16 @@
-set(UN_PROJECT_NAME "UltraNoteWallet")
-set(UN_CURRENCY_DISPLAY_NAME "UltraNote")
-set(UN_CURRENCY_TICKER "XUN")
-set(UN_VERSION 1.0.4-beta)
-set(UN_COPYRIGHT "Copyright (c) 2017-2018 UltraNote")
-set(UN_VENDOR "UltraNote Foundation")
-set(UN_IDENTIFIER "org.ultranote.UltraNoteWallet")
-set(UN_CONTACT "https://ultranote.org/")
-set(UN_SUMMARY "UltraNote XUN wallet")
-set(UN_DESCRIPTION "UltraNote XUN wallet\
- UltraNote is a decentralized privacy protected peer-to-peer\
- cryptocurrency and untraceable encrypted messaging system. UltraNote is\
- open-source; its design is public, nobody owns or controls UltraNote and\
+set(UN_PROJECT_NAME "DarwinCoinWallet")
+set(UN_CURRENCY_DISPLAY_NAME "DarwinCoin")
+set(UN_CURRENCY_TICKER "DWC")
+set(UN_VERSION 1.0beta)
+set(UN_COPYRIGHT "Copyright (c) 2018 DarwinCoin")
+set(UN_VENDOR "DarwinCoin Foundation")
+set(UN_IDENTIFIER "org.DarwinCoin.DarwinCoinWallet")
+set(UN_CONTACT "https://DarwinCoin.org/")
+set(UN_SUMMARY "DarwinCoin DWC wallet")
+set(UN_DESCRIPTION "DarwinCoin DWC wallet\
+ DarwinCoin is a decentralized privacy protected peer-to-peer\
+ cryptocurrency and untraceable encrypted messaging system. DarwinCoin is\
+ open-source; its design is public, nobody owns or controls DarwinCoin and\
  everyone can take part.\
  .\
  Features of the UltraNote wallet listed bellow:\
