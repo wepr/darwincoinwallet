c60aef2cf4dffb129d1656b45c25abd3c48c7a17
wepr <we-pr@yandex.ru>
2018-02-21 17:13:09 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ using namespace WalletGui;
 
 int main(int argc, char* argv[]) {
   QApplication app(argc, argv);
-  app.setApplicationName("UltraNotewallet");
+  app.setApplicationName("DarwinCoinwallet");
   app.setApplicationVersion(Settings::instance().getVersion());
   app.setQuitOnLastWindowClosed(false);
 
